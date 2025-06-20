9f3ebc906895b06b3c93be26dee292cd531ba346
jerry <jerry270039@gmail.com>
2025-06-21 00:35:32 +0800

    0.0.0.0的使用1

--- express/server.js
+++ express/server.js
@@ -1,13 +1,33 @@
 import express from "express";
+import { readFile } from "node:fs/promises";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/:idolName", async (req, res) => {
+  //  /:idolName
+  const idolName = req.params.idolName;
+
+  const idolDataText = await readFile("data.json", "utf-8");
+  const idolData = JSON.parse(idolDataText);
+
+  const resultIdol = idolData.find(
+    (idol) => idol.name.toLowerCase() === idolName.toLowerCase()
+  );
+
+  if (!resultIdol) {
+    res.status(404).send("404 Not Found");
+  }
+
+  return res.status(200).json(resultIdol);
 });
+
+// app.listen(port, "0.0.0.0", () => {
+//   //local ip address: 127.0.0.1
+//   console.log(`Example app listening on http://127.0.0.1:${port}`);
+// });
 
 app.listen(port, () => {
   //local ip address: 127.0.0.1
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on http://127.0.0.1:${port}`);
 });
