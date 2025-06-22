e60fd0c5399d6978c70f8270281c8726b771cb93
jerry <jerry270039@gmail.com>
2025-06-22 22:54:05 +0800

    恢复了被错误删除的学习记录,学了点理论

--- express/backend-app/server.js
+++ express/backend-app/server.js
@@ -1,12 +1,18 @@
 import express from "express";
+import { readFile } from "node:fs/promises";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/todos", async (_req, res) => {
+  const todosData = await readFile("./data.json", "utf-8");
+  const todos = JSON.parse(todosData);
+  return res.status(200).json(todos);
+  // req未被使用，可在req前加上 _
 });
 
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://127.0.0.1:${port}`);
 });
